bb78f8eb7ca607c13a77c168a24e867be7c6ddb9
ro-el-c <chung3286@naver.com>
2024-05-08 16:19:28 +0900

    feat: default theme 화면 구현

--- breakout.js
+++ breakout.js
@@ -1,1 +1,14 @@
 // 웹프로그래밍 10팀
+
+window.onload = pageLoad;
+
+function pageLoad () {
+  $("#side-menu").css({display: "none"});
+  
+}
+
+
+/*
+$("#side-menu").css({display: "block"}); 하면서
+$("#code-area").css({width: "calc(100vw - 250px"}); 설정
+*/
