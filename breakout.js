3f5851d1ed41c681ad6dbf6cd50216b5867c4dea
ro-el <chung3286@naver.com>
2024-05-09 11:48:35 +0900

    feat: 환경 설정 마무리시 화면 변화 구현

--- breakout.js
+++ breakout.js
@@ -5,14 +5,11 @@ window.onload = pageLoad;
 function pageLoad() {
   selectSetting();
 
-  $("#side-menu").css({ display: "none" });
-  $("#user-setting input.setting-select-input").change(selectSetting);
+  //환경 설정
+  $("#side-menu").css({ display: "none" }); //사용자 환경 설정시 사이드 메뉴 숨김
+  $("#user-setting input.setting-select-input").change(selectSetting); //환경 설정 버튼 선택시 css style 변화
+  $("#to-next-btn").on("click", completeSetting); //환경 설정 마무리
 }
-
-/*
-$("#side-menu").css({display: "block"}); 하면서
-$("#code-area").css({width: "calc(100vw - 250px"}); 설정
-*/
 
 function selectSetting() {
   $("#user-setting input.setting-select-input").each(function () {
@@ -34,4 +31,18 @@ function selectSetting() {
       });
     }
   });
+}
+
+function completeSetting() {
+  $("#user-setting").css({ display: "none" });
+  $("#side-menu").css({ display: "block" });
+  $("#code-area").css({ width: "calc(100vw - 300px" });
+
+  /*TODO
+    환경 설정 이후,
+    난이도 선택창 띄우기 및 사이드 메뉴에 사용자가 선택한 환경 설정 정보 표시하ㄱ기
+    - 사용자 이름
+    - 테마
+    - 공 모양
+  */
 }
