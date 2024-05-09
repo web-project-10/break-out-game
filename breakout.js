5fd33ba141f394be701d7c2f4ce4c71911507b13
ro-el-c <chung3286@naver.com>
2024-05-09 11:29:41 +0900

    feat: 사용자 게임 환경 설정창 구현

--- breakout.js
+++ breakout.js
@@ -2,13 +2,36 @@
 
 window.onload = pageLoad;
 
-function pageLoad () {
-  $("#side-menu").css({display: "none"});
-  
-}
+function pageLoad() {
+  selectSetting();
 
+  $("#side-menu").css({ display: "none" });
+  $("#user-setting input.setting-select-input").change(selectSetting);
+}
 
 /*
 $("#side-menu").css({display: "block"}); 하면서
 $("#code-area").css({width: "calc(100vw - 250px"}); 설정
 */
+
+function selectSetting() {
+  $("#user-setting input.setting-select-input").each(function () {
+    const checked = $(this).prop("checked");
+    const inputId = $(this).attr("id");
+    const $label = $(`label[for=${inputId}]`);
+
+    if (checked) {
+      console.log(inputId);
+      $label.css({
+        "background-color": "#71929d",
+        color: "white",
+      });
+    } else {
+      console.log(inputId);
+      $label.css({
+        "background-color": "#fff",
+        color: "#000",
+      });
+    }
+  });
+}
